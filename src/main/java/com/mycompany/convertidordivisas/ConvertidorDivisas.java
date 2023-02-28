8ad4a9f69641e82b30a8628e444f63643fe55893
Hector <hector_ing2018@hotmail.com>
2023-02-27 21:11:33 -0300

    primer comit

---
+++ src/main/java/com/mycompany/convertidordivisas/ConvertidorDivisas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.convertidordivisas;
+
+/**
+ *
+ * @author Hector
+ */
+public class ConvertidorDivisas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
